eb8ebcbb5824cf46a3fc86e9d544a986ac7d35d3
A. Tellez <alsetema@gmail.com>
2020-04-19 19:40:45 +0200

    its all garbage, yes, mixed casing styles and stuff like that :)

---
+++ udpserver/CMakeFiles/Anyname.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Anyname"
+  "Anyname.pdb"
+  "CMakeFiles/Anyname.dir/udpserver.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Anyname.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
